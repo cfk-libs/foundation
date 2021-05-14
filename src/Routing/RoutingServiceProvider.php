2f2e606f89f0f71712a204690a26e184f614ecb6
Cemre Fatih Karakulak <cradexco@gmail.com>
2021-05-14 23:34:38 +0300

    carry

--- src/Routing/RoutingServiceProvider.php
+++ src/Routing/RoutingServiceProvider.php
@@ -2,14 +2,6 @@
 
 namespace Sellony\Routing;
 
-use Slim\Factory\AppFactory;
-use Slim\CallableResolver;
-use Psr\Http\Message\ResponseFactoryInterface;
-use Slim\Interfaces\CallableResolverInterface;
-use Slim\Interfaces\RouteCollectorInterface;
-use Slim\Interfaces\RouteResolverInterface;
-use Slim\Routing\RouteCollector;
-use Slim\Routing\RouteResolver;
 use Illuminate\Support\ServiceProvider;
 
 class RoutingServiceProvider extends ServiceProvider
@@ -21,26 +13,6 @@ class RoutingServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->instance(
-            ResponseFactoryInterface::class,
-            $responseFactory = AppFactory::determineResponseFactory()
-        );
-
-        $this->app->instance(
-            CallableResolverInterface::class,
-            $callableResolver = new CallableResolver($this->app)
-        );
-
-        $this->app->instance(
-            RouteCollectorInterface::class,
-            $routeCollector = new RouteCollector($responseFactory, $callableResolver, $this->app)
-        );
-
-        $this->app->instance(
-            RouteResolverInterface::class,
-            $routeCollector = new RouteResolver($routeCollector)
-        );
-
         // $this->app->singleton('router', function ($app) {
         //     return new Router($app['events'], $app);
         // });
