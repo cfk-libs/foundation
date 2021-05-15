3f14455af7017b0ebb05c837478f096a22e7483b
Cemre Fatih Karakulak <cradexco@gmail.com>
2021-05-15 12:20:08 +0300

    carry

--- src/helpers.php
+++ src/helpers.php
@@ -61,6 +61,18 @@ if (! function_exists('path')) {
     }
 }
 
+if (! function_exists('app_path')) {
+    /**
+     * Get the path to the application folder.
+     *
+     * @param  string  $path
+     * @return string
+     */
+    function app_path($path = '')
+    {
+        return app()->path($path);
+    }
+}
 
 if (! function_exists('public_path')) {
     /**
